7ec773ef58c6c68c90d4fd9aef2729ec47fc65be
Hamza Hasbi <hamza.hasbi@gmail.com>
2020-07-31 20:11:21 +0100

    feat: Paypal processor

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    `gatsby-env-variables`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,32 +1,77 @@
-import React from "react"
+import React, {useState} from "react"
+
+import CustomPopUp from "./CustomPopUp";
+import PaypalButton from "./Payment"
 import { PricingCard } from "tabler-react"
 
 const Pricing = ({ offer }) => {
-  return (
-    <PricingCard className="cards">
-      <PricingCard.Category>{offer.name}</PricingCard.Category>
-      <PricingCard.Price>{`$${offer.price}`} </PricingCard.Price>
-      <PricingCard.AttributeList>
-        <PricingCard.AttributeItem hasIcon available={offer.bgMusic}>
-          {"Background Music"}
-        </PricingCard.AttributeItem>
-        <PricingCard.AttributeItem hasIcon available={offer.isVoice}>
-          {"Voice Over"}
-        </PricingCard.AttributeItem>
-        <PricingCard.AttributeItem hasIcon available={offer.revision}>
-          {`Revisions(${offer.numRevison})`}
-        </PricingCard.AttributeItem>
-        <PricingCard.AttributeItem>
-          <strong>{offer.numWords} </strong>
-          {"word"}
-        </PricingCard.AttributeItem>
 
-        <PricingCard.AttributeItem>
-          <strong> Delivery in {offer.delay} </strong>
-        </PricingCard.AttributeItem>
-      </PricingCard.AttributeList>
-      <button className="choose">{"Choose plan"} </button>
-    </PricingCard>
+  const [processed, setProcessed] = useState(-1);
+  const closePopUp = () => {
+    setProcessed(-1);
+  }
+  const onSuccess = (payment) => {
+    setProcessed(1);
+
+  }	        
+  const onCancel = (data) => {
+      setProcessed(0);
+
+  }	        
+  const onError = (err) => {
+      console.log("Error!", err);
+      setProcessed(0); 
+  }		
+  const client = {
+    sandbox:    PAYPAL_CLIENT_ID_SANDBOX, // sandbox client ID
+    production: PAYPAL_CLIENT_ID_PRODUCTION,// production client ID
+  };     
+  const currency = 'USD'; // or you can set this value from your props or state   
+  console.log(PAYPAL_ENV_DEV);
+  return (
+      <PricingCard className="cards">
+        <PricingCard.Category>{offer.name}</PricingCard.Category>
+        <PricingCard.Price>{`$${offer.price}`} </PricingCard.Price>
+        <PricingCard.AttributeList>
+          <PricingCard.AttributeItem hasIcon available={offer.bgMusic}>
+            {"Background Music"}
+          </PricingCard.AttributeItem>
+          <PricingCard.AttributeItem hasIcon available={offer.isVoice}>
+            {"Voice Over"}
+          </PricingCard.AttributeItem>
+          <PricingCard.AttributeItem hasIcon available={offer.revision}>
+            {`Revisions(${offer.numRevison})`}
+          </PricingCard.AttributeItem>
+          <PricingCard.AttributeItem>
+            <strong>{offer.numWords} </strong>
+            {"word"}
+          </PricingCard.AttributeItem>
+
+          <PricingCard.AttributeItem>
+            <strong> Delivery in {offer.delay} </strong>
+          </PricingCard.AttributeItem>
+        </PricingCard.AttributeList>
+        {/* <button className="choose">{"Choose plan"} </button> */}
+        <PaypalButton 
+        currency={currency}
+        total={offer.price}
+        onError={onError}
+        onSuccess={onSuccess}
+        onCancel={onCancel}
+        client={client}
+        env={PAYPAL_ENV_DEV}
+        />
+        {processed === 1 &&       
+        <CustomPopUp popUpText="You're Payment has been processed succefully we will reach out to you soon" 
+        closePopUp={closePopUp} 
+        PopUpButton="YEY!!"/>
+        }
+
+        {processed === 0 && 
+          <CustomPopUp popUpText="Payment Cancelled !" closePopUp={closePopUp} PopUpButton="Understood"/>
+        }
+      </PricingCard>
   )
+   
 }
 export default Pricing
