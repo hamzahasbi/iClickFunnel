da46293eb65f8eef727a7c4c0d6585905fa691a2
Hamza Hasbi <hamza.hasbi@gmail.com>
2020-08-03 12:50:32 +0100

    chore: branding email

--- env/production.js
+++ env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     PAYPAL_CLIENT_ID_SANDBOX : "AQIl4KVkA_ZTOIVH4NUm_MeHIcpbNHnQdW-dTdFAaugX6rcKJNRiYBlzi4OmGWw-qbH2QYZmfwgMpcqZ",
     PAYPAL_CLIENT_ID_PRODUCTION : "ARZsE1weSGneOGDpOJ-WE1pFgJGNfSEbixNkCR4GyQJR-yuaM2LybFqRYlz9GG45K4Ru8kyqiFyhzlbP",
-    MAILTO : "hamza.hasbi@gmail.com",
+    MAILTO : "service.easyanimate@gmail.com",
     PAYPAL_ENV_DEV : "production",
 }
