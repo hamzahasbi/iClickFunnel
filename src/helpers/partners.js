379b4ef3c0865ccb5b87a83d6d6c1a069641d765
Hamza Hasbi <hamza.hasbi@gmail.com>
2020-09-10 13:06:58 +0100

    feat: adds

---
+++ src/helpers/partners.js
@@ -0,0 +1,22 @@
+const affiliateLinks = [
+  {
+    partner: "Doodle Maker",
+    link: "https://jvz4.com/c/1372271/357045",
+    description:
+      "Futuristic Artificial Intelligence Technology Automatically Transforms Any Text or Content Into Colorful Doodle Videos In Any Language\n",
+  },
+  {
+    partner: "Animation Studio",
+    link: "https://jvz4.com/c/1372271/306929",
+    description:
+      "The Animation Creator That You Have Been\n Waiting For Has Finally Arrived...",
+  },
+  {
+    partner: "Stream Store Cloud",
+    link: "https://jvz6.com/c/1372271/344565",
+    description:
+      "Build Unlimited Stores \n You Want To Achieve Success A Lot Quicker And Easier..",
+  },
+]
+
+export default affiliateLinks
